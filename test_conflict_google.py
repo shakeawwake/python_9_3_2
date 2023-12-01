46707d241f26150e41d089354705f081bb1c3ddd
kglazova <kglazova@huntflow.ru>
2023-12-01 17:29:56 +0300

    changed google->duckduckgo

--- test_conflict_google.py
+++ test_conflict_google.py
@@ -3,6 +3,6 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open('https://google.com')
+browser.open('https://duckduckgo.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
 browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
