8d5cf323ca6c59ef3e720d9288f86924366e7b10
kglazova <kglazova@huntflow.ru>
2024-02-14 10:14:14 +0300

    changed google->yahoo.com

--- test_conflict_google.py
+++ test_conflict_google.py
@@ -3,6 +3,6 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open('https://mail.com')
+browser.open('https://yahoo.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
 browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
